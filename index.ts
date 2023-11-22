8ac4cec29c327145d1ab029dc3ffce49964bc23e
Anderson <1824974+0x7An@users.noreply.github.com>
2023-11-22 15:05:05 -0300

    Refactor main script with enhanced error handling

--- index.ts
+++ index.ts
@@ -1,83 +1,78 @@
 import { config } from "dotenv";
-import { ThirdwebSDK } from "@thirdweb-dev/sdk";
-import { readFileSync } from "fs";
+import { SmartContract, ThirdwebSDK } from "@thirdweb-dev/sdk";
 import { Goerli } from "@thirdweb-dev/chains";
-import { SmartWallet, SmartWalletConfig } from "@thirdweb-dev/wallets";
+import { SmartWallet } from "@thirdweb-dev/wallets";
 import { LocalWalletNode } from "@thirdweb-dev/wallets/evm/wallets/local-wallet-node";
+import { BaseContract } from "ethers";
 
 config();
 
 const chain = Goerli;
-const factoryAddress = "0x3c115305ecC8119D9517BB67a5B3D4073Bc9CffF";
+const factoryAddress = "0x0EDfbA441f630E0D38F8d92C486EF0e9119E100a";
 const secretKey = process.env.THIRDWEB_SECRET_KEY as string;
 
-const main = async () => {
-  if (!secretKey) {
-    throw new Error(
-      "No API Key found, get one from https://thirdweb.com/dashboard"
-    );
+if (!secretKey) {
+  throw new Error("No API Key found. Get one from https://thirdweb.com/dashboard");
+}
+
+const initializePersonalWallet = async () => {
+  try {
+    const personalWallet = new LocalWalletNode();
+    await personalWallet.loadOrCreate({ strategy: "encryptedJson", password: "password" });
+    return personalWallet;
+  } catch (error: any) {
+    throw new Error("Failed to initialize personal wallet: " + error.message);
   }
-  console.log("Running on", chain.slug, "with factory", factoryAddress);
+};
 
-  // Load or create personal wallet
-  // here we generate LocalWallet that will be stored in wallet.json
-  const personalWallet = new LocalWalletNode();
-  await personalWallet.loadOrCreate({
-    strategy: "encryptedJson",
-    password: "password",
-  });
+const setupSmartWallet = async (personalWallet: LocalWalletNode) => {
+  try {
+    const smartWallet = new SmartWallet({ chain, factoryAddress, secretKey, gasless: true });
+    await smartWallet.connect({ personalWallet });
+    return smartWallet;
+  } catch (error: any) {
+    throw new Error("Failed to setup smart wallet: " + error.message);
+  }
+};
 
-  const personalWalletAddress = await personalWallet.getAddress();
-  console.log("Personal wallet address:", personalWalletAddress);
+const initializeSDK = async (smartWallet: SmartWallet) => {
+  try {
+    return await ThirdwebSDK.fromWallet(smartWallet, chain, { secretKey });
+  } catch (error: any) {
+    throw new Error("Failed to initialize SDK: " + error.message);
+  }
+};
 
-  // Configure the smart wallet
-  const config: SmartWalletConfig = {
-    chain,
-    factoryAddress,
-    secretKey,
-    gasless: true,
-  };
+const performClaims = async (smartWallet: SmartWallet, tokenContract: SmartContract<BaseContract>, editionContract: SmartContract<BaseContract>) => {
+  try {
+    const tx1 = await tokenContract.erc20.claim.prepare(1);
+    const tx2 = await editionContract.erc1155.claim.prepare(0, 1);
+    const batchTx = await smartWallet.executeBatch([tx1, tx2]);
+    console.log("Claimed 1 ERC20 token & 1 Edition NFT, tx hash:", batchTx.receipt.transactionHash);
+  } catch (error: any) {
+    throw new Error("Failed to claim tokens: " + error.message);
+  }
+};
 
-  // Connect the smart wallet
-  const smartWallet = new SmartWallet(config);
-  await smartWallet.connect({
-    personalWallet,
-  });
+const main = async () => {
+  try {
+    console.log("Running on", chain.slug, "with factory", factoryAddress);
 
-  // now use the SDK normally to perform transactions with the smart wallet
-  const sdk = await ThirdwebSDK.fromWallet(smartWallet, chain, {
-    secretKey: secretKey,
-  });
+    const personalWallet = await initializePersonalWallet();
+    console.log("Personal wallet address:", await personalWallet.getAddress());
 
-  console.log("Smart Account addr:", await sdk.wallet.getAddress());
-  console.log("balance:", (await sdk.wallet.balance()).displayValue);
+    const smartWallet = await setupSmartWallet(personalWallet);
+    const sdk = await initializeSDK(smartWallet);
 
-  // Claim a ERC20 token
-  const tokenContract = await sdk.getContract(
-    "0xc54414e0E2DBE7E9565B75EFdC495c7eD12D3823" // TokenDrop on goerli
-  );
-  const editionContract = await sdk.getContract(
-    "0x8096C71f400984C3C1B7F3a79Ab0C0EaC417b91c"
-  ); // EditionDrop on goerli
-  const tokenBalance = await tokenContract.erc20.balance();
-  const editionBalance = await editionContract.erc1155.balance(0);
-  console.log("ERC20 token balance:", tokenBalance.displayValue);
-  console.log("Edition NFT balance:", editionBalance.toNumber());
+    console.log("Smart Account addr:", await sdk.wallet.getAddress());
+    console.log("balance:", (await sdk.wallet.balance()).displayValue);
 
-  // Execute multiple transactions at once
-  const tx1 = await tokenContract.erc20.claim.prepare(1);
-  const tx2 = await editionContract.erc1155.claim.prepare(0, 1);
-  const transactions = [tx1, tx2];
-
-  const batchTx = await smartWallet.executeBatch(transactions);
-  console.log(
-    "Claimed 1 ERC20 token & 1 Edition NFT, tx hash:",
-    batchTx.receipt.transactionHash
-  );
-  const tokenBalanceAfter = await tokenContract.erc20.balance();
-  const editionBalanceAfter = await editionContract.erc1155.balance(0);
-  console.log("ERC20 token balance:", tokenBalanceAfter.displayValue);
-  console.log("Edition NFT balance:", editionBalanceAfter.toNumber());
+    const tokenContract = await sdk.getContract("0xc54414e0E2DBE7E9565B75EFdC495c7eD12D3823");
+    const editionContract = await sdk.getContract("0x8096C71f400984C3C1B7F3a79Ab0C0EaC417b91c");
+    await performClaims(smartWallet, tokenContract, editionContract);
+  } catch (error: any) {
+    console.error(error.message);
+  }
 };
 
 main();
